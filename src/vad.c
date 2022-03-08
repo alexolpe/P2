4b8cdf0180e8234ad89e7b536e74f8bb25ecec97
Alexandre Olive Pellicer <alexandreolivepellicer@gmail.com>
2022-03-08 14:35:36 +0100

    prova

--- src/vad.c
+++ src/vad.c
@@ -98,7 +98,7 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   switch (vad_data->state) {
   case ST_INIT:
     vad_data->state = ST_SILENCE;
-    vad_data->p1=f.p+10; //em posat un umbral de 10
+    vad_data->p1=f.p+8; //em posat un umbral de 10
     break;
 
   case ST_SILENCE:
